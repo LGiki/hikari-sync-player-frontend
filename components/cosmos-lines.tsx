85d10387f5cc158a9216bcbdff98ba0b9b02ca7e
LGiki <qq547221432@gmail.com>
2022-04-16 00:21:51 +0800

    update index page

---
+++ components/cosmos-lines.tsx
@@ -0,0 +1,11 @@
+import {memo} from "react"
+import {css} from "@emotion/css"
+import {ASSETS_BASE_URL} from "../util/constants";
+
+export default memo(function CosmosLines() {
+    return <div className={css`
+      height: 235px;
+      width: 100%;
+      background-image: url(${`${ASSETS_BASE_URL}/images/cosmos_line.svg`});
+    `}/>
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
+import {useCallback, useEffect} from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Set the value of css variable `--app-height` to window.innerHeight
+  const setAppSize = useCallback(() => {
+    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+  }, [])
+
+  useEffect(() => {
+    setAppSize();
+    window.addEventListener('resize', setAppSize)
+    return () => {
+      window.removeEventListener('resize', setAppSize)
+    }
+  }, [setAppSize])
+
+  return <>
+    <Head>
+      <meta name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
