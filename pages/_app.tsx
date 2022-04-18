da68c0afe81317e8bfc77882b148a91fa6645fcf
LGiki <qq547221432@gmail.com>
2022-04-18 22:10:51 +0800

    1. set app height css variable when loaded or page resized 2. add react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,42 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import Head from "next/head"
 import {useCallback, useEffect} from "react";
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from "react-toastify";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  // Set the value of css variable `--app-height` to window.innerHeight
-  const setAppSize = useCallback(() => {
-    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
-  }, [])
+function MyApp({Component, pageProps}: AppProps) {
+    // Set the value of css variable `--app-height` to window.innerHeight
+    const setAppSize = useCallback(() => {
+        document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }, [])
 
-  useEffect(() => {
-    setAppSize();
-    window.addEventListener('resize', setAppSize)
-    return () => {
-      window.removeEventListener('resize', setAppSize)
-    }
-  }, [setAppSize])
+    useEffect(() => {
+        setAppSize();
+        window.addEventListener('resize', setAppSize)
+        return () => {
+            window.removeEventListener('resize', setAppSize)
+        }
+    }, [setAppSize])
 
-  return <>
-    <Head>
-      <meta name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
-    </Head>
-    <Component {...pageProps} />
-  </>
+    return <>
+        <Head>
+            <meta name="viewport"
+                  content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
+        </Head>
+        <Component {...pageProps} />
+        <ToastContainer
+            position="bottom-center"
+            autoClose={3000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
+    </>
 }
 
 export default MyApp
