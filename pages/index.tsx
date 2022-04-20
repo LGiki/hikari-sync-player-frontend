ff6909782348bc9bad9d07f1128c14200fec699f
LGiki <qq547221432@gmail.com>
2022-04-20 11:13:05 +0800

    1. request to API and navigate to player page 2. style improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,11 @@ import Button from "../components/button"
 import {css, keyframes} from "@emotion/css"
 import Input from "../components/input"
 import CosmosLines from "../components/cosmos-lines";
+import {useState} from "react";
+import {toast} from "react-toastify";
+import {useRouter} from "next/router";
+import Head from "next/head";
+import {API_BASE_URL} from "../util/constants";
 
 const heartAnimation = keyframes`
   0%, 100% {
@@ -23,6 +28,9 @@ const heartAnimation = keyframes`
 `
 
 const Home: NextPage = () => {
+    const [url, setUrl] = useState('')
+    const router = useRouter()
+
     return (
         <div
             className={css`
@@ -34,6 +42,9 @@ const Home: NextPage = () => {
               justify-content: center;
             `}
         >
+            <Head>
+                <title>✨ Hikari Listen Together</title>
+            </Head>
             <div
                 className={css`
                   flex: 1;
@@ -51,7 +62,7 @@ const Home: NextPage = () => {
                       top: 0;
                       left: 0;
                       width: 100%;
-                      z-index: -1;
+                      z-index: 0;
                     `}
                 >
                     <CosmosLines/>
@@ -60,6 +71,7 @@ const Home: NextPage = () => {
                   display: flex;
                   flex-direction: column;
                   width: 80%;
+                  z-index: 1;
                 `}>
                     <div className={css`
                       text-align: center;
@@ -69,8 +81,56 @@ const Home: NextPage = () => {
                     `}>
                         ✨ Hikari Listen Together
                     </div>
-                    <Input margin={{top: 10}} placeholder='https://'/>
-                    <Button margin={{top: 10}}>确定</Button>
+                    <Input
+                        margin={{top: 10}}
+                        placeholder='https://'
+                        value={url}
+                        onChange={(newValue) => setUrl(newValue)}
+                    />
+                    <Button
+                        margin={{top: 10}}
+                        onClick={() => {
+                            if (url.length === 0) {
+                                toast.error('请输入链接')
+                                return
+                            }
+
+                            if (!url.startsWith('https://') && !url.startsWith('http://')) {
+                                toast.error('请检查输入链接是否正确')
+                                return
+                            }
+
+                            if (url.includes('xiaoyuzhoufm.com') && !url.includes('xiaoyuzhoufm.com/episode')) {
+                                toast.error('请输入小宇宙单集链接')
+                                return
+                            }
+
+                            if (url.includes('cowtransfer.com') && !url.includes('cowtransfer.com/s')) {
+                                toast.error('请输入奶牛快传分享链接')
+                                return
+                            }
+
+                            fetch(`${API_BASE_URL}/api/v1/room`, {
+                                method: 'POST',
+                                body: JSON.stringify({
+                                    "url": url
+                                })
+                            })
+                                .then(res => res.json())
+                                .then(json => {
+                                    if (json['code'] !== 200) {
+                                        toast.error(`错误：${json['msg']}`)
+                                    } else {
+                                        router.push(`/player/${json['data']['type']}/${json['data']['roomId']}`)
+                                    }
+                                })
+                                .catch(() => {
+                                    toast.error('发送请求失败，请稍后再试')
+                                })
+                        }}
+                    >
+                        确定
+                    </Button>
                 </div>
             </div>
             <footer
@@ -81,9 +141,22 @@ const Home: NextPage = () => {
                   align-items: center;
                   margin-bottom: 5px;
                   font-size: .9rem;
+                  z-index: 1;
                 `}
             >
-                <p>请在上方填入小宇宙单集链接</p>
+                <p>请在上方填入
+                    <span className={css`
+                      box-shadow: inset 0px -5px 0 0 rgba(133, 214, 244, .7);
+                    `}>
+                        小宇宙单集链接
+                    </span>
+                    或
+                    <span className={css`
+                      box-shadow: inset 0 -5px 0 0 rgba(253, 218, 101, .7);
+                    `}>
+                        奶牛快传分享链接
+                    </span>
+                </p>
                 <p>Made with
                     <span
                         className={css`
