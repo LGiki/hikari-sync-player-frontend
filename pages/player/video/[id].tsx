09e18c4e1aeb182bd53f1960c229da987b8817a1
LGiki <qq547221432@gmail.com>
2022-04-25 21:51:58 +0800

    WIP: video player

---
+++ pages/player/video/[id].tsx
@@ -0,0 +1,53 @@
+import {GetServerSidePropsContext} from "next";
+import {css} from "@emotion/css";
+import {API_BASE_URL} from "../../../util/constants";
+
+function VideoPlayer(props: {
+    url: string
+}) {
+
+    return <div>
+        <video
+            className={css`
+              height: var(--app-height);
+              width: 100vw;
+            `}
+            crossOrigin={'anonymous'}
+            src={props.url}
+            preload='auto'
+            controls={true}
+            autoPlay={false}
+        />
+    </div>
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const {id} = context.query
+
+    const data = await fetch(`${API_BASE_URL}/room/${id}`)
+        .then(res => res.json())
+        .catch(() => {
+            return {
+                redirect: {
+                    destination: '/',
+                    permanent: false
+                }
+            }
+        })
+    if (data.code !== 200) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+    return {
+        props: {
+            ...data.data.mediaData
+        }
+    }
+}
+
+
+export default VideoPlayer
