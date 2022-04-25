9c1ea126136dfae985953938d483a3e7e6077cdc
LGiki <qq547221432@gmail.com>
2022-04-25 14:40:12 +0800

    add player util

--- util/player.ts
+++ util/player.ts
@@ -1,3 +1,12 @@
+import {nanoid} from "nanoid";
+
 export function secondsToTime(seconds: number) {
+    if (isNaN(seconds)) {
+        return '00:00:00'
+    }
     return new Date(1000 * seconds).toISOString().substring(11, 11 + 8)
+}
+
+export function generateUserId() {
+    return nanoid(9)
 }
